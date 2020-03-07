2283947b737a66858931a98de3404f1430465b12
xuxl <xuxl@citex.io>
2020-03-07 12:06:05 +0800

    commit

--- Promise/simple.js
+++ Promise/simple.js
@@ -1,63 +1,51 @@
-const PENDING = 'pending';
-const FULFILLED = 'fulfilled';
-const REJECTED = 'rejected';
+const pending = 'pending';
+const resolved = 'resolved';
+const Rejected = 'Rejected';
+
 class Promise{
     constructor(fn){
-        //当前状态
-        this.state = PENDING;
-        //终值
+        this.state = pending;
         this.value = null;
-        //拒绝原因
         this.reason = null;
-        //成功状态回调对列
-        this.onFulfilledCallbacks = [];
-        //拒绝状态回调对列
-        this.onRejectedCallBacks = [];
 
-        //成功态回调
+        this.onResolvedCallbacks=[];
+        this.onRejectedCallbacks=[];
+
         const resolve = value =>{
-            // 使用macro-task机制(setTimeout),确保onFulfilled异步执行,且在 then 方法被调用的那一轮事件循环之后的新执行栈中执行。
-            setTimeout(() =>{
-                if(this.state===PENDING){
-                    //将pending状态变成fulfilled态，保证调用次数不超过一次
-                    this.state = FULFILLED;
-                    //终值
+            setTimeout(()=>{
+                if(this.state === pending){
+                    this.state = resolved;
                     this.value = value;
-                    this.onFulfilledCallbacks.map(cb=>{
+                    this.onResolvedCallbacks.map(cb=>{
                         this.value = cb(this.value);
                     })
                 }
-            });
-        };
-        const reject  = reason => {
-            // 使用macro-task机制(setTimeout),确保onFulfilled异步执行,且在 then 方法被调用的那一轮事件循环之后的新执行栈中执行。
-            setTimeout(() =>{
-                if(this.state===PENDING){
-                    //将pending状态变成fulfilled态，保证调用次数不超过一次
-                    this.state = REJECTED;
-                    //终值
+            })
+        }
+        const reject = reason =>{
+            setTimeout(()=>{
+                if(this.state === pending){
+                    this.state = Rejected;
                     this.reason = reason;
-                    this.onRejectedCallBacks.map(cb=>{
+                    this.onResolvedCallbacks.map(cb=>{
                         this.reason = cb(this.reason);
                     })
                 }
-            });
-        };
+            })
+        }
         try{
-            fn(resolve,reject);
+            fn(resolve,reject)
         }catch(e){
-            reject(e);
+            reject(e)
         }
     }
-    then(onFulfilled,onRejected){
-        if(typeof onFulfilled==='function'){
-            //加人对列中
-            this.onFulfilledCallbacks.push(onFulfilled);
+    then(onResolved,onRejected){
+        if(typeof onResolved==='function'){
+            this.resolvedCallbacks.push(onResolved)
         }
         if(typeof onRejected==='function'){
-            this.onRejectedCallbacks.push(onRejected);
+            this.RejectedCallbacks.push(onRejected)
         }
-         // 返回this支持then 方法可以被同一个 promise 调用多次
-         return this;
+        return this
     }
 }
